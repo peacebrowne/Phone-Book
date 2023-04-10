51fd99de3e955438997e501d0665045f199665aa
peacebrowne <emmanuelbrowne751999@gmail.com>
2023-04-10 11:16:46 -0400

    completed add,edit and delete

--- app.js
+++ app.js
@@ -1,346 +1,295 @@
-const banner = document.querySelector('.banner')
-const hideEle = ele => ele.style.display = 'none';
-const showEle = ele => ele.style.display = 'flex';
-const popup_banner = document.querySelector('.popup-banner')
-const contact_form = document.querySelectorAll('.contact-form input')
-const desktop_view = document.querySelector('.deskop-view table tbody')
-const mobile_view = document.querySelector('.mobile-view table')
-const delete_form = document.querySelector('.delete-contact')
-const add_form = document.querySelector('.contact-form')
 
-let transaction,target;
-let phoneBook = [];
+/**
+ * Searches for an html element based on specified attribute value [class or id]
+ * @param {*} ele 
+ * @returns {object} - an element
+ */
+const find_element = ele => {
+    return document.querySelector(`${ele}`)
+}
 
-// Handling all click events.
-document.body.addEventListener('click', ev => {
-    ev.preventDefault()
-    target = ev.target;
+const storage = data => localStorage.setItem('db',JSON.stringify(data))
+const get_storage = () => {
+    return JSON.parse(localStorage.getItem('db'))
+}
 
-    if(target.className.includes('open-book')) open_book();
-    else if(target.className.includes('add-contact')) toggle('add',popup_banner,'display-flex');
-    else if(target.className.includes('cancel-form')) cancel_form();
-    else if(target.className.includes('save-btn')) {
-        if(transaction == 'add'){
-            add_contact(contact_form)
-        }else if(transaction == 'edit'){
-            edit_contact(contact_form)
-        }
-    }
-    else if(target.className.includes('mobile-contact-info')) mobile_contact_info(target);
-    else if(target.className.includes('edit-btn')) edit_target(target)
-    else if(target.className.includes('delete-btn')) delete_target(target);
-    else if(target.className.includes('yes-btn')) delete_contact()
-    
+const reset_form = form => form.forEach(ele => ele.value = '');
+
+// Database
+let PHONEBOOK = []
+const form = find_element('.popup-banner')
+let mode = 'add'
+
+/**
+ * Toggle a css style on the specified html element. 
+ * If class value is available it removes it else it adds it
+ * @param {*} ele 
+ * @param {*} clas 
+ * @returns {void} - an element
+ */
+const toggleEl = (ele,clas) => ele.classList.toggle(clas)
+const contact_list = find_element('.contact-list ul')
+
+/**
+ * Handles all click event
+ */
+document.addEventListener('click', event => {
+
+    const targetEl = event.target;
+    const targetElClas = Array.from(targetEl.classList)
+
+    if(targetElClas.includes('open-book')) open_phoneBook(targetEl)
+    if(targetElClas.includes('add-contact-btn')) display_form(targetEl)
+    if(targetElClas.includes('edit-contact-btn')) stabler(targetEl) 
+    if(targetElClas.includes('delete-contact-btn')) delete_contact(targetEl) 
+    if(targetElClas.includes('exit-form')) toggleEl(form,'hidden')
+    if(targetElClas.includes('save-btn')) mode === 'add' ? add_contact(form_validation()) : edit_contact(form_validation())
+
 })
 
 /**
- * Display and hide forms depending on user interation
- * between add, edit or delete contact
- *  *  @param msg - transction message
- *  *  @param ele - popup banner
- *  *  @param clas - class to toggle
+ * Open phone book
+ * @param {*} target 
+ * @returns {void} - an element
  */
-const toggle = (msg,ele,clas) =>{
+const open_phoneBook = target => {
 
-    ele.classList.toggle(clas);
-    transaction = msg;
-    if(msg =='add' || msg == 'edit'){
-
-        hideEle(delete_form)
-        ele.children[1].classList.toggle(clas)
-
-    }else if(msg == 'delete'){
-        hideEle(add_form)
-        ele.children[2].classList.toggle(clas)
-    }
+    const element = target.closest('.banner')
+    toggleEl(element,'hidden')
 
 }
 
-/**
- * Remove toggled class from each forms if present or not 
- * and hide popup banner
- *  *  @param element - popup banner
- *  *  @param clas - class to toggle
- */
-const remove_toggle = (element,clas) =>{
-
-    [element.children[1],element.children[2]].forEach(ele =>{
-        ele.classList.remove('display-flex')
-    });
-    element.classList.toggle(clas)
-}
-
-// Opens phone book
-const open_book = ()=>  setTimeout(()=>hideEle(banner),1000);
-
-// Cancel and reset form
-const cancel_form = () =>{
-    reset_form(contact_form)
-    remove_toggle(popup_banner,'display-flex')
-}
-
-let number = 1, index = 0;
-
-/**
- * Display all contacts on mobile and desktop view 
- * They both have differnt interfaces
- *  *  @param {object} - object of new contact
- * 
- */
-const display = () =>{
-        mobile_view.innerHTML = '' , desktop_view.innerHTML = ''
-        number = 1
-
-    phoneBook.forEach(contact => {
-
-        // mobile view display
-        const mobile_display = `
-        <tbody class="mobile-row">
-            <tr class="contact-row">
-                <td class="mobile-contact-info">${number}</td>
-                <td class="mobile-contact-info name">${contact.name}</td>
-                <td class="mobile-contact-info opt"><i class="fa fa-angle-down mobile-contact-info"></i></td>
-            </tr>
-            <tr class="mobile-list"  id="${contact.id}" data-index="${index}">
-                <td colspan="3">
-                    <li>${contact.contact}</li>
-                    <li>${contact.email}</li>
-                    <li>
-                        <button class="edit-btn">Edit</button>
-                        <button class="delete-btn">Delete</button>
-                    </li>
-                </td>
-            </tr>
-        </tbody>
-        `
-        // desktop view display
-        const desktop_display = `
-        <tr class="contact-row desktop-row"  data-index="${index}" id="${contact.id}">
-            <td>${number}</td>
-            <td class="name">${contact.name}</td>
-            <td>${contact.contact}</td>
-            <td>${contact.email}</td>
-            <td><button class="edit-btn">Edit</button></td>
-            <td><button class="delete-btn">Delete</button></td>
-        </tr>
-        `
-        number++ , index++
-
-        mobile_view.innerHTML += mobile_display
-        desktop_view.innerHTML += desktop_display
-
-    })
-
-    // number = 1, index = 0;
-
-}
-
-const angles = {
-    down:'fa-angle-down',
-    up:'fa-angle-up'
-}
-
-/** 
-*   when phonebook is open on mobile provides user with different interaction
-*  *   @param target- either of tr or td elements
-*/
-const mobile_contact_info = target =>{
-
-    let contact_info = target.parentElement;
-    let contact;
-    if(contact_info.className.includes('contact-row')){
-
-        contact = contact_info.parentElement.children[1]
-        contact.classList.toggle('mobile-list-on')
-
-    }else{
-
-        contact = contact_info.parentElement.parentElement.children[1]
-        contact.classList.toggle('mobile-list-on')
-
-    }
-        
-}
-
-/**
- * Getting data from contact form and validating.
- * If form is not complete, cancel form execution,
- * else store new contact into phone book.
- *  *  @param data [html input collection]- contact form data
- * 
- */
-const add_contact = data =>{
-    let contact = {};
-
-    for(datum of data){
-        if(datum.name != 'email' && datum.value === '')return
-        // if (datum.value === '') return;
-        contact[datum.name] = datum.value
-    }
-    contact['id'] = number;
-
-    reset_form(data)
-    phoneBook.push(contact)
-    localStorage.setItem('phoneBook',JSON.stringify(phoneBook))
-    display()
-
-}
-
-
-/**
- *  Edit target fetch targeted contact and prepare contact for edit...
- *  *  @param data [html input collection]- contact form data
- */
+// const photoEl = find_element('.upload-photo')
+let photo = ''
 let id;
-let element;
-let contact;
 
-const edit_target = data =>{
+const upload_photo = event =>{
 
-    element = data.parentElement.parentElement;
+    const element = event;
+    const photo_path = URL.createObjectURL(element.files[0])
+    element.closest('.upload-photo').nextElementSibling.innerHTML = `<img src="${photo_path}" alt="">`
+    photo = photo_path;
 
-    if(element.className.includes('contact-row')){
-        id = element.id;
-        contact = phoneBook.find(ele => ele.id == id)
-    }else{
-        id = element.parentElement.id
-        contact = phoneBook.find(ele => ele.id == id)
-    }
-
-    contact_form[0].value = contact.name;
-    contact_form[1].value = contact.contact;
-    contact_form[2].value = contact.email;
-
-    toggle('edit',popup_banner,'display-flex')
 }
 
-/**
- * If form is not complete, cancel form execution,
- * else add replace edited contact in phone book.
- *  *  @param data [html input collection]- contact form data
- * 
- */
-const edit_contact = data =>{
+// photoEl.addEventListener('change',upload_photo)
 
-    let target_contact = {};
+function form_validation() {
 
-    for(datum of data){
-        if (datum.name != 'email' && datum.value === '') return;
-        target_contact[datum.name] = datum.value
+    const inputs = find_element('form').querySelectorAll('input')
+    const data = {}
+    const contact = {}
+
+    for(let i = 1; i < inputs.length; i++){
+
+        const ele = inputs[i]
+
+        if(ele.name === 'orange' || ele.name === 'lonestar') contact[ele.name] = ele.value
+        if(!ele.value) return
+        else data[ele.name] = ele.value
+        
+    };
+
+    data.contacts = contact
+    data.photo = photo
+    data.note = find_element('textarea').value
+    reset_form([...inputs,find_element('textarea')])
+    find_element('.photo').innerHTML = ''
+    return data
+
+}
+
+const add_contact = data => {
+
+    if(!data) return;
+    data.id = PHONEBOOK.length+1 * (Math.floor(Math.random() * 100))
+    PHONEBOOK.push(data)
+    storage(PHONEBOOK)
+    display_contact(data)
+    mode = 'add'
+
+}
+
+
+function stabler(target) {
+
+    mode = 'edit'
+    id = contact_id(target)
+    const contact = PHONEBOOK.find(val => val.id === id)
+    display_form(contact)
+    id = ''
+
+}
+
+function contact_id(targetEl){
+    return +targetEl.closest('.contact-item').dataset.id
+}
+
+
+function delete_contact(targetEl){
+
+    id = contact_id(targetEl)
+    for (let i = 0; i < PHONEBOOK.length; i++) {
+        if(PHONEBOOK[i].id === id) {
+
+            PHONEBOOK.splice(i,1)
+            storage(PHONEBOOK)
+            targetEl.closest('.contact-item').remove()
+
+        }
     }
-    target_contact['id'] = id;
+}
 
-    for (let i = 0; i < phoneBook.length; i++) {
-        const item = phoneBook[i];
-        if (item.id == id) {
-            phoneBook[i] = target_contact;
+
+function edit_contact(data){
+    
+    for (let i = 0; i < PHONEBOOK.length; i++) {
+        data.id = id
+
+        if(PHONEBOOK[i].id === data.id) {
+
+            PHONEBOOK[i] = data
+            contact_list.querySelector('.name').textContent = PHONEBOOK[i].name
+            contact_list.querySelector('.email').textContent = PHONEBOOK[i].email
+            storage(PHONEBOOK)
+            mode = 'add'
+            return;
         }
         
     }
-    localStorage.setItem('phoneBook',JSON.stringify(phoneBook))
-    display()
-
 }
 
-/**
- * Delete contact from db and remove it from screen.
- *  *  @param data [html input collection]- contact form data
- * 
- */
+function display_form (data) {
+    const contact_form = find_element('.popup-banner').querySelector('form');
+    
+    if(mode === 'add'){
+        contact_form.innerHTML = `
+           
+        <div class="upload-photo">
+        <i class="fa fa-camera"></i>
+        <input type="file" onchange="upload_photo(this)" name="photo" id="">
+        </div>
 
- const delete_target = data =>{
+        <div class="form-group photo"></div>
 
-    element = data.parentElement.parentElement; 
+        <div class="form-group">
+            <input type="text" name="name" required placeholder="Name">
+        </div>
+        <div class="form-group">
+            <input type="email" name="email" placeholder="Email">
+        </div>
+        
+        <div class="form-group">
+            <input type="tel" name="orange"  placeholder="Orange Number" maxlength="10">
+        </div>
 
-    if(element.className.includes('contact-row')){
-        id = element.id;
-        contact = phoneBook.find(ele => ele.id == id)
+        <div class="form-group">
+            <input type="tel" name="lonestar"  placeholder="Lonestar Number" maxlength="10">
+        </div>
+
+        <div class="form-group">
+            <input type="date" name="data">
+        </div>
+
+        <div class="form-group">
+            <textarea name="" id="" cols="30" rows="3" required placeholder="Note"></textarea>
+        </div>
+
+        <div class="form-group">
+            <button type="button" name="" id="" class="save-btn">Save</button>
+        </div>
+        `
     }else{
-        id = element.parentElement.id
-        contact = phoneBook.find(ele => ele.id == id)
+        contact_form.innerHTML = `
+        <div class="upload-photo">
+        <i class="fa fa-camera"></i>
+        <input type="file" onchange="upload_photo(this)" name="photo" id="">
+        </div>
+
+        <div class="form-group photo">
+            <img src="${data.photo} " />
+        </div>
+
+        <div class="form-group">
+            <input type="text" value="${data.name}" name="name" required placeholder="Name">
+        </div>
+        <div class="form-group">
+            <input type="email" value="${data.email}" name="email" placeholder="Email">
+        </div>
+        
+        <div class="form-group">
+            <input type="tel" name="orange" value="${data.contacts.orange}"  placeholder="Orange Number" maxlength="10">
+        </div>
+
+        <div class="form-group">
+            <input type="tel" name="lonestar" value="${data.contacts.lonestar}"  placeholder="Lonestar Number" maxlength="10">
+        </div>
+
+        <div class="form-group">
+            <input type="date" value="${data.data}" name="data">
+        </div>
+
+        <div class="form-group">
+            <textarea name="" id="" cols="30" rows="3" required placeholder="Note">${data.note}</textarea>
+        </div>
+
+        <div class="form-group">
+            <button type="button" name="" id="" class="save-btn">Save</button>
+        </div>
+        `
     }
 
-    toggle('delete',popup_banner,'display-flex')
+    toggleEl(form,'hidden')
 
 }
 
-const delete_contact = () => {
+function display_contact(data){
+    contact_list.insertAdjacentHTML(
+        "beforeend"
+        ,
 
-    for (let i = 0; i < phoneBook.length; i++) {
-        const item = phoneBook[i];
-        if (item.id == id) {
-            phoneBook.splice(i,1);
-        }
-    }
-    localStorage.setItem('phoneBook',JSON.stringify(phoneBook))
-    display()
-    remove_toggle(popup_banner,'display-flex')
+        ` <li class="contact-item" data-id="${data.id}">
+        <img class="contact-photo" src="${data.photo}" alt="">
+        <span class="contact-info">
+            <h5 class="name">${data.name}</h5>
+            <h6 class="email">${data.email}</h6>
+        </span>
+        <span class="contact-action">
+            <i class="fa fa-ellipsis-vertical"></i>
+            <div class="contact-action-menu">
+                <ul>
+                    <li class="edit-contact-btn">
+                        <i class="fa fa-pen"></i>
+                        <span>Edit</span>
+                    </li>
+                    <li class="delete-contact-btn">
+                        <i class="fa fa-trash"></i>
+                        <span>Delete</span>
+                    </li>
+                </ul>
+            </div>
+        </span>
+    </li>`
+    )
 }
 
-/**
- * Removing old data from forms and resetting it to normal.
- *  *  @param data [html input collection]- contact form input
- */
-const reset_form = data => data.forEach(datum => datum.value = '')
+const search = find_element('.search')
+search.addEventListener('keydown', search_contact)
 
-/**
- * Search contact 
- *  *  @param key - any keyboard character
- * 
- */
-const search = document.querySelector('.search')
+function search_contact(event){
+    const key = event.key.toLowerCase();
+    Array.form(contact_list).forEach(contact => {
 
-const search_contact = ev => {
-
-    const char = ev.target.value.toLocaleLowerCase()
-    const names = document.querySelectorAll('.name')
-
-    names.forEach(name => {
-
-        let element = name.parentElement
-        let text = name.textContent.toLocaleLowerCase()
-
-        if(text.includes(char)){
-
-            if(element.className.includes('desktop-row')){
-                
-                element.style.display = 'table-row'
-            }
-
-            if(element.parentElement.className.includes('mobile-row')){
-                element.parentElement.style.display = 'table-row-group'
-            }
-
-        }
-        else{
-            if(element.parentElement.className.includes('mobile-row')){
-                element.parentElement.style.display = 'none'
-            }else{
-                element.style.display = 'none'
-            }
-        }
-
+        // if(contact.te)
     })
-
-}
-
-search.addEventListener('keyup',search_contact)
-
-/**
-    fetching all contacts from database to display
-    when the page is loading.
-    if contact exist, display it, else move on
-*/
-window.onload = ()=>{
-        if(localStorage.getItem('phoneBook') == null){
-            localStorage.setItem('phoneBook',JSON.stringify([]))
-        }else{
-            phoneBook = JSON.parse(localStorage.getItem('phoneBook'))
-        }
-
-        display()
 }
 
 
+window.onload = () => {
 
+    if (!get_storage()) storage(PHONEBOOK)
+    PHONEBOOK = get_storage()
+    if(!get_storage()) return
+    PHONEBOOK.forEach(contact => display_contact(contact))
 
+}
